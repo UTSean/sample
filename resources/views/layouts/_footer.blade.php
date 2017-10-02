ec2c6ff9290b460ef33402a78d8b8cf32ec74464
Sean <yutongsean@qq.com>
2017-10-02 10:14:26 +0000

    Finish layout and routes

---
+++ resources/views/layouts/_footer.blade.php
@@ -0,0 +1,13 @@
+<div class="col-md-12">
+    <footer class="footer">
+        <samll class="slogon">
+            <img class="brand_icon" src="" alt="">
+            <a href="">Hello</a>
+        </samll>
+        <nav>
+            <ul>
+                <li><a href="{{route('about')}}">about us</a></li>
+            </ul>
+        </nav>
+    </footer>
+</div>
--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -1,4 +1,11 @@
 @extends('layouts.default')
 @section('content')
-<h1>主页</h1>
+<div class="jumbotron">
+    <h1>Hello Laravel</h1>
+    <p class="lead">
+        What you see is the homepage of my first Web App.
+    </p>
+    <p>Everything will begin with it.</p>
+    <p><a class="btn btn-lg btn-success" href="{{route('signup')}}" role="button">Register now!</a></p>
+</div>
 @stop
